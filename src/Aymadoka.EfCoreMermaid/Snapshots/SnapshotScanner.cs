c3a9f90546b4e6420eb6112f5410f1fc00b74c2f
Aymadoka <upgrader@qq.com>
2025-07-04 07:53:05 +0800

    feat: optimized code.

--- src/Aymadoka.EfCoreMermaid/Snapshots/SnapshotScanner.cs
+++ src/Aymadoka.EfCoreMermaid/Snapshots/SnapshotScanner.cs
@@ -12,7 +12,7 @@ namespace Aymadoka.EfCoreMermaid.Snapshots
     public static class SnapshotScanner
     {
         /// <summary>
-        /// 获取当前应用程序域中所有继承自 <see cref="ModelSnapshot"/> 的非抽象类类型
+        /// 扫描当前 AppDomain 中所有已加载的程序集，查找所有继承自 <see cref="ModelSnapshot"/> 的非抽象类类型
         /// </summary>
         /// <returns>所有继承自 <see cref="ModelSnapshot"/> 的类型列表</returns>
         public static List<Type> GetAllModelSnapshotDerivedTypes()
@@ -20,26 +20,31 @@ namespace Aymadoka.EfCoreMermaid.Snapshots
             // 获取所有已加载的程序集
             var assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
-            var modelSnapshotType = typeof(ModelSnapshot);
-            var derivedTypes = assemblies
-                .SelectMany(a =>
-                {
-                    try
-                    {
-                        // 获取程序集中的所有类型
-                        return a.GetTypes();
-                    }
-                    catch (ReflectionTypeLoadException ex)
-                    {
-                        // 某些类型加载失败时，返回已成功加载的类型
-                        return ex.Types.Where(t => t != null)!;
-                    }
-                })
-                // 过滤出继承自 ModelSnapshot 的非抽象类
-                .Where(t => t != null && t.IsClass && !t.IsAbstract && modelSnapshotType.IsAssignableFrom(t))
-                .ToList();
+            var allTypes = assemblies
+                 .SelectMany(a =>
+                 {
+                     try
+                     {
+                         // 获取程序集中的所有类型
+                         return a.GetTypes();
+                     }
+                     catch (ReflectionTypeLoadException ex)
+                     {
+                         // 某些类型加载失败时，返回已成功加载的类型
+                         return ex.Types.OfType<Type>();
+                     }
+                 });
 
-            return derivedTypes;
+            var modelSnapshotType = typeof(ModelSnapshot);
+
+            // 查找所有继承自 ModelSnapshot 的非抽象类类型
+            var derivedTypes = from t in allTypes
+                               where t != null && t.IsClass && !t.IsAbstract
+                                    && modelSnapshotType.IsAssignableFrom(t)
+                               select t;
+
+            var result = derivedTypes.ToList();
+            return result;
         }
     }
 }
